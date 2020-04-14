0dbfdc980b6a3e47dea5412e741083c742898e09
unknown <G00387867@gmit.ie>
2020-04-14 15:30:14 +0100

    importing required libraries and setting path to read from iris file in repository

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# Adam Shaat
+
+import pandas as pd # importing pandas library
+import matplotlib.pyplot as plt  # importing matplotlib library
+import seaborn as sns # importing seaborn library
+pd.read_csv("https://raw.githubusercontent.com/G00387867/pands-project/master/iris.data") # recalling the iris file from own repository
+iris = pd.read_csv("https://raw.githubusercontent.com/G00387867/pands-project/master/iris.data")
